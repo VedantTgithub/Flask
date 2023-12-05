cdf37dbe3722424c73ca6120854ec441ceb7c150
Vedant <vedant0711@gmail.com>
2023-12-06 01:36:38 +0530

    new commit

---
+++ main.py
@@ -0,0 +1,141 @@
+from flask import Flask, flash, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+import json
+from datetime import datetime
+from flask import session
+
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+
+
+local_server = True
+app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = params['gmail-user'],
+    MAIL_PASSWORD=  params['gmail-password']
+)
+mail = Mail(app)
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+db = SQLAlchemy(app)
+
+
+app.secret_key = 'super-secret-key'
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+
+
+
+@app.route("/")
+def home():
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params, posts=posts)
+
+@app.route("/dashboard", methods=['GET', 'POST'])
+def dashboard():
+    if "user" in session and session['user'] == params['admin_user']:
+        posts = Posts.query.all()
+        return render_template("dashboard.html", params=params, posts=posts)
+
+    if request.method == "POST":
+        username = request.form.get("uname")
+        userpass = request.form.get("upass")
+        if username == params['admin_user'] and userpass == params['admin_password']:
+            # set the session variable
+            session['user'] = username
+            posts = Posts.query.all()
+            return render_template("dashboard.html", params=params, posts=posts)
+        else:
+            flash('Invalid credentials! Please try again.', 'danger')  # Example: using Flask flash messages
+    return render_template("login.html", params=params)
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect("/dashboard")    
+
+@app.route("/delete/<string:sno>" , methods=['GET', 'POST'])
+def delete(sno):
+    if "user" in session and session['user']==params['admin_user']:
+        post = Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect("/dashboard")
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html',params=params)
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    print("Received post_slug:", post_slug)
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
+
+
+@app.route("/contact",methods = ['GET', 'POST'])
+def contact():
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
+        db.session.add(entry)
+        db.session.commit()
+       
+    return render_template('contact.html',params=params)
+
+@app.route("/edit/<string:sno>" , methods=['GET', 'POST'])
+def edit(sno):
+   
+
+    if "user" in session and session['user'] == params['admin_user']:
+        if request.method == "POST":
+            title = request.form.get('title')
+            slug = request.form.get('slug')
+            content = request.form.get('content')
+            date = datetime.now()
+
+            if sno == '0':
+                post = Posts(title=title, slug=slug, content=content, date=date)
+                db.session.add(post)
+                db.session.commit()
+            
+            else:
+             post = Posts.query.filter_by(sno=sno).first()
+             post.title = title
+             post.slug = slug
+             post.content = content
+             post.date = date
+             db.session.commit()
+             return redirect('/edit/'+sno)
+
+    post = Posts.query.filter_by(sno=sno).first()
+    return render_template('edit.html', params=params, post=post)
+
+
+app.run(debug=True)
